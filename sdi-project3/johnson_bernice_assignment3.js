22040c06a364485eb55470ca62f9759e7eab9872
Bernice Johnson <bernicejohnson@fullsail.edu>
2012-06-11 19:04:14 -0400

    Placed my story in the order I would like it in comment style.//

--- sdi-project3/johnson_bernice_assignment3.js
+++ sdi-project3/johnson_bernice_assignment3.js
@@ -2,3 +2,65 @@
 //Bernice Johnson
 //June 10, 2012
 //Deliverable 3
+
+
+//GLOBAL VARIABLES
+
+var 
+
+
+
+
+
+
+//1# Day 2. We decide to go on an Old Town Trolley Tour of Washington D.C.//I head down to the lobby 
+//to buy some tickets and get more info. (Procedure? conditional?)
+
+
+
+
+
+
+
+
+
+//2# Buy the tickets//how many?//how much? what time?//Math. return number. ticket cost? Boolean?
+
+
+
+
+
+
+//3# Get on trolley-OBJECT?//Array? stops, return array? array.push?//WHILE LOOP
+
+
+
+
+
+
+//4# We get off at the white house, take a pic of us in front of it. walk back and buy an italian ice and water.//string? return string?
+
+
+//5# We get back in line for the trolley ride to finish the tour.//FOR LOOP//ARRAY?
+
+
+
+//6# The tour was fun. Now we are back in our room ready to relax and order dinner. We decide to order Sushi. return??
+
+
+
+
+
+//Function.Method Calls//
+//1#
+//2#
+//3#
+//4#
+//5#
+//6#
+
+/////END////////////////
+
+
+
+

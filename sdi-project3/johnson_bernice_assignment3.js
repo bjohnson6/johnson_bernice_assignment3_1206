0a69399286747187522e26d2c48f590b9f40dc1e
Bernice Johnson <bernicejohnson@fullsail.edu>
2012-06-12 19:19:07 -0400

    Added some global variables. and started my 1# function trolleyRideInfo. added properties.

--- sdi-project3/johnson_bernice_assignment3.js
+++ sdi-project3/johnson_bernice_assignment3.js
@@ -6,15 +6,44 @@
 
 //GLOBAL VARIABLES
 
-var 
+var town = "Washington D.C.",
+    weatherCheck = "First we need to check the weather",
+    trolleyTimeInfo = "then head down to the lobby to get more info about the time the next trolley leaves.";
 
 
+//ADD LOCAL VARIABLES
 
-
-
+console.log("We would like to go on a Trolley Tour of " + town + " today." + weatherCheck + trolleyTimeInfo);
 
 //1# Day 2. We decide to go on an Old Town Trolley Tour of Washington D.C.//I head down to the lobby 
-//to buy some tickets and get more info. (Procedure? conditional?)
+//to get more info on the time it arrives and possibly buy the tickets. Are we in time for the next tour? Is the weather good? (Method Function? conditional?)
+var trolleyRideInfo = {
+    "name": "Ole Town Trolley Tour", //string property
+    "where": "lobby",  //string proprty
+    "whatTime": 2,    //number property
+    "goodWeather": true, //boolean property
+    "costPerTicket": 38, //number property
+    "manAtDeskName": "Sonny",
+    "niceWeather": Function(niceDay){  //METHOD FUNCTION//BOOLEAN ARGUMENT//ARGUMENT CONDITIONAL
+    	 if (niceDay === true) {
+    	 	console.log("The weather is nice enough outside to go on the " + this.name);
+    	 	return(true);
+    	 }else{
+    	 	console.log("Looks like we will have to wait until tomorrow when the weather is nicer.");
+    	 	return(false);
+    	 }
+
+};
+
+trolleyRideInfo.niceWeather(true);
+
+
+
+
+};
+
+
+
 
 
 
@@ -39,7 +68,7 @@ var
 
 
 //4# We get off at the white house, take a pic of us in front of it. walk back and buy an italian ice and water.//string? return string?
-
+//OR MAKE THIS A NESTED LOOP TO THE WHILE LOOP???
 
 //5# We get back in line for the trolley ride to finish the tour.//FOR LOOP//ARRAY?
 

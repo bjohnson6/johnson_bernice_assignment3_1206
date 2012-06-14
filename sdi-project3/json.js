950c43c97233f73184c9b49b8807e5885350d569
Bernice Johnson <bernicejohnson@fullsail.edu>
2012-06-14 13:58:41 -0400

    json wasn't showing up.

--- sdi-project3/json.js
+++ sdi-project3/json.js
@@ -2,6 +2,60 @@
 //JSON file
 //Deliverable 3
 
-var json ={
+var json ={var json ={
+	"trolleyRide" : {
+		"01" : {
+	      "vehicle" : "trolley", //object property?
+	      "tourGuide" : "Loo Loo" //string property
+	     }, 
+	     "02" : {
+	       "destinationStopsLoopOne" : 
+	       [ //array property
+	       "Woodley Park", 
+	       "National Zoo", 
+	       "Washington National Cathedral",
+	       "George Town",
+	       "Lincoln Memorial",
+	       "Museams"]
+	      },
+	      "03" : {
+	        "destinationStopsLoopTwo" : 
+	        [ //array property
+	        "Union Station",
+	        "Washington Momument",
+	        "White House"
+	         ],
+	       }
+	         
+	    }
+};
+
+var json2 ={
+	"trolleyRide" : [
+		"01" : {
+	      "vehicle" : "trolley", //object property?
+	      "tourGuide" : "Loo Loo" //string property
+	     }, 
+	     "02" : {
+	       "destinationStopsLoopOne" : 
+	       [ //array property
+	       "Woodley Park", 
+	       "National Zoo", 
+	       "Washington National Cathedral",
+	       "George Town",
+	       "Lincoln Memorial",
+	       "Museams"]
+	      },
+	      "03" : {
+	        "destinationStopsLoopTwo" : 
+	        [ //array property
+	        "Union Station",
+	        "Washington Momument",
+	        "White House"
+	         ],
+	       }
+	         
+	    ]
+};
 	
 };
